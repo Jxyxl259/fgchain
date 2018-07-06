738d9b221524f5a50f02a6e5a554befbe81554af
jxyxl <jxy_259_job@163.com>
2018-07-06 19:54:52 +0800

    update

--- src/main/java/com/fgchain/main/module/front/login/LoginController.java
+++ src/main/java/com/fgchain/main/module/front/login/LoginController.java
@@ -1,10 +1,14 @@
 package com.fgchain.main.module.front.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class LoginController {
 
+	private static Logger log = LoggerFactory.getLogger(LoginController.class);
 
+	
 
 }
