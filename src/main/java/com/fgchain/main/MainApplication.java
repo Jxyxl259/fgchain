7f0d4aa164581e907e86ecba00767c23f270e54e
jiangxy7 <jiangxy7@lenovo.com>
2018-09-02 14:39:37 +0800

    update: test jenkins

--- src/main/java/com/fgchain/main/MainApplication.java
+++ src/main/java/com/fgchain/main/MainApplication.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
         "com.fgchain.main.module.**.dao"
 })
 public class MainApplication {
-	// 测试jenkins构建13
+	// 测试jenkins构建14
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
