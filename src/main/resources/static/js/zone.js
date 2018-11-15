592ee2befca38079f76141a334005db30b99b6b9
jiangxy7 <jiangxy7@lenovo.com>
2018-11-16 00:51:15 +0800

    用户管理页面开整

--- src/main/resources/static/js/zone.js
+++ src/main/resources/static/js/zone.js
@@ -1,9 +1,4 @@
 /* 后台页面加载时执行 */
 $(function(){
 
-    $("#zone_menu_tree_container_div li").click(function(){
-        var iframe_url = $(this).children("a").attr("content");
-        $("#zone_iframe_detail_container_div").attr("src", iframe_url);
-    });
-
 });
