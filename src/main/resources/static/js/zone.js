cdc57e620fc661b919f9629df7883ebfcad0eeab
jiangxy7 <jiangxy7@lenovo.com>
2018-11-17 01:35:07 +0800

    后台页面css样式优化

--- src/main/resources/static/js/zone.js
+++ src/main/resources/static/js/zone.js
@@ -1,4 +1,20 @@
 /* 后台页面加载时执行 */
 $(function(){
 
+    // 先向后台请求用户的权限数据
+    // 设置为全局变量
+    // 作为参数传入菜单收缩函数
+    // 函数中对数据做处理，生成展开菜单
+
+    /* 左侧菜单栏展示js */
+    $("#zone_menu_tree_container_div").mouseover(function(){
+        $("#zone_menu_tree_container_div").removeClass("menu_shrink");
+        $("#zone_iframe_detail_container_div").removeClass("iframe_expansion");
+    });
+
+    $("#zone_menu_tree_container_div").mouseout(function(){
+        $("#zone_menu_tree_container_div").addClass("menu_shrink");
+        $("#zone_iframe_detail_container_div").addClass("iframe_expansion");
+    });
+
 });
