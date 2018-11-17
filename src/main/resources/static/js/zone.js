33be0df6cf09d716263edb17faa6d13f76c1c607
jiangxy7 <jiangxy7@lenovo.com>
2018-11-17 12:25:52 +0800

    zone 页面menu菜单效果优化

--- src/main/resources/static/js/zone.js
+++ src/main/resources/static/js/zone.js
@@ -7,14 +7,19 @@ $(function(){
     // 函数中对数据做处理，生成展开菜单
 
     /* 左侧菜单栏展示js */
-    $("#zone_menu_tree_container_div").mouseover(function(){
-        $("#zone_menu_tree_container_div").removeClass("menu_shrink");
-        $("#zone_iframe_detail_container_div").removeClass("iframe_expansion");
+    $("div.menu_L1,#zone_menu_tree_container_div").mouseover(this,function(){
+        if(this.id == "zone_menu_tree_container_div"){
+            $("#zone_menu_tree_container_div").removeClass("menu_shrink");
+            $("#zone_iframe_detail_container_div").removeClass("iframe_expansion");
+        }else{
+            $($(this).children().eq(1)).addClass("in");
+        }
     });
 
     $("#zone_menu_tree_container_div").mouseout(function(){
         $("#zone_menu_tree_container_div").addClass("menu_shrink");
         $("#zone_iframe_detail_container_div").addClass("iframe_expansion");
+        $(".menu_L2").removeClass("in").attr("aria-expanded")
     });
 
 });
